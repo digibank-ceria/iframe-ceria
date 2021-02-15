fe0c08d6c997abd5f1616c010e3ad77338e91e50
ivanharsono <ivananandaharsono@gmail.com>
2021-02-15 10:41:11 +0700

    Updates

---
+++ precache-manifest.393b990a1b87ee7da879fbc9a69eecc2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbfb4a368b1c59fdc2ee2b999a979d39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63bf85cefc40e45eeecb",
+    "url": "/static/js/2.fb900f82.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.fb900f82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa26fa338acb68b3f7c6",
+    "url": "/static/js/main.1477944a.chunk.js"
+  },
+  {
+    "revision": "89cea73da49992847ce6",
+    "url": "/static/js/runtime-main.5e5966c0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/iframe-ceria/precache-manifest.a37faa7a98e6f6c4878554438696148a.js"
+  "/precache-manifest.393b990a1b87ee7da879fbc9a69eecc2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/iframe-ceria/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
