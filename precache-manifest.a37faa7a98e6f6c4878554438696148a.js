93f3d19c3e5bc3188e3cb464e98aaca86aec52ce
ivanharsono <ivananandaharsono@gmail.com>
2021-02-15 10:39:20 +0700

    Updates

---
+++ precache-manifest.a37faa7a98e6f6c4878554438696148a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e44d0b5fcce1805c54f6f0fdb5226a0",
+    "url": "/iframe-ceria/index.html"
+  },
+  {
+    "revision": "63bf85cefc40e45eeecb",
+    "url": "/iframe-ceria/static/js/2.fb900f82.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/iframe-ceria/static/js/2.fb900f82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa26fa338acb68b3f7c6",
+    "url": "/iframe-ceria/static/js/main.1477944a.chunk.js"
+  },
+  {
+    "revision": "82b7ae3e0767b9ebab36",
+    "url": "/iframe-ceria/static/js/runtime-main.55a85c4e.js"
+  }
+]);
